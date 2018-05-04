6d72f5eec2ff2e7fa4024af9f65e7d8697debec7
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-05-04 13:54:34 -0600

    1: Adds page components
    Generates default About, Contact, Home, Legal, and Projects components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { LegalComponent } from './legal/legal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    LegalComponent
   ],
   imports: [
     BrowserModule
