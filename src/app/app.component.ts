beed44f2da0fd5d2544a9225fe82dcd35a5a30d8
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-05-07 21:15:20 -0600

    2: Nav URLs now built from Routes
    The routing table now builds the navigation list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'George Nikolai Kotula';
+  routes: object[];
+
+  constructor(router: Router) {
+    this.routes = [];
+    let builder: string = "";
+
+    // Build the strings based on the paths, but capitalize them:
+    router.config.forEach(route => {
+      if ((route.path !== undefined) && (route.path !== "")) {
+        builder = "";
+        builder += route.path.charAt(0).toUpperCase();
+        builder += route.path.substr(1, route.path.length - 1);
+        this.routes.push({path : route.path, name : builder});
+      }
+    });
+  }
 }
