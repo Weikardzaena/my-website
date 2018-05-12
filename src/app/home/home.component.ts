ff79a6a890a766e8b7258408cfa5a622b2176576
Weikardzaena <g.nikolai.kotula@gmail.com>
2018-05-12 12:44:03 -0600

    9: Format Home page and add content
    The primary Home page content and basic layout is added.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LINK_GITHUB, LINK_LINKEDIN } from '../external-links';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  linkGitHub = LINK_GITHUB;
+  linkLinkedIn = LINK_LINKEDIN;
   constructor() { }
 
   ngOnInit() {
